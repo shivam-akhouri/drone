b8651d316f099bd24bc2b0df10d6cc3cdd10f72f
Shivam Akhouri <shivamakhouri1142@gmail.com>
2023-04-13 01:53:59 +0530

    cors removed

--- index.js
+++ index.js
@@ -8,11 +8,7 @@ const port = process.env.PORT||5000;
 
 const cors = require("cors");
 app.use(cors())
-var io = require("socket.io")(http, {
-    cors: {
-        origin: "<http://localhost:3000>"
-    }
-}) 
+var io = require("socket.io")(http) 
 
 app.get('/', (req, res)=>{
     res.send("Connection is established")
